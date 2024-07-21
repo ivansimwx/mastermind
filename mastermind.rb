3b361250a33e050a850b33b14843dcbe27322eb8
ivansimwx <16796516+ivansimwx@users.noreply.github.com>
2024-07-21 15:57:00 +0800

    Create mastermind file with psuedo code

---
+++ mastermind.rb
@@ -0,0 +1,73 @@
+#game
+
+##select who guess
+puts " Who will be the code maker? Type 'me' to be the code maker or 'com' for the computer to be the code maker'
+codemaker = gets.chomp.lowercase.to_i
+codermaker.check valid ?
+
+#check valid selection for guesses?
+if "me" human 
+if "com" computer
+else redo select guess
+
+#generate secret code if computer is generating
+array of 4 with each index being a random_from_4_colors (blue, green, orange purple)
+
+#manually selet secret code if human is generating
+
+
+#guess attempt
+puts "What is your guess?"
+puts "input the colours in sequence using the first letter"
+puts "there are only 4 colours, Blue, Green, Orange, Purple
+puts "for e.g. Blue, Green, Orange, Purple should be sent as "bgop"
+guess = gets.chomp
+check.guess
+guess.lowercase.to_i
+guess.split
+
+#check guess by human is valid
+guess only includes alphabets b,g,o,p
+if valid, guess count add by one and return true
+if not valid, puts "Your guess did not include the appropriate alphabets for colours" and redo guess
+
+#generate secret code if computer is generating
+array of 4 with each index being a random_from_4_colors (blue, green, orange purple)
+
+#manually selet secret code if human is generating
+
+
+#guess attempt
+puts "What is your guess?"
+puts "input the colours in sequence using the first letter"
+puts "there are only 4 colours, Blue, Green, Orange, Purple
+puts "for e.g. Blue, Green, Orange, Purple should be sent as "bgop"
+guess = gets.chomp
+check.guess
+guess.lowercase.to_i
+guess.split
+
+#check guess by human is valid
+guess only includes alphabets b,g,o,p
+if valid, guess count add by one and return true
+if not valid, puts "Your guess did not include the appropriate alphabets for colours" and redo guess
+
+#check outcome?
+- if <12 guesses and guess is correct, puts "victory message"
+-if 12 guesses and guess is incorrect, puts "loss message"
+
+#feedback guess
+feedback_arr = []
+check how many colours are present
+for every colour in the guess that is present and in the right position, increase black peg number
+    for every color in the guess that is present but in the right position, increase white peg
+append black peg and white peg to feedback_arr
+print feedback_arr.join
+
+loop
+#new game created + intro
+#guess attempt
+#check guess
+#check outcome?
+- if <12 guesses and guess is correct
+-if not, feedback guess
