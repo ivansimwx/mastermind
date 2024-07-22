b846da64f3de8a2a0da1a9b8c058a664ab79caed
ivansimwx <16796516+ivansimwx@users.noreply.github.com>
2024-07-22 22:26:10 +0800

    Move main (mastermind.rb) ruby file out of library

--- lib/mastermind.rb
+++
@@ -1,9 +0,0 @@
-require_relative "computer"
-require_relative "game_controller"
-require_relative "player"
-require "colorize"
-
-computer = Computer.new
-player = Player.new
-game = GameController.new(player, computer)
-game.play
